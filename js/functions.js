9d8b0478b89566ec1ec3d0d76473ee66b5b34c08
javier estrada <estrada.javier.2022@gmail.com>
2020-12-14 20:51:39 -0600

    completed all exercises

--- js/functions.js
+++ js/functions.js
@@ -2,8 +2,13 @@ alert('Welcome to my Website!');
 
 "use strict";
 
+        // example of using a IFFE
+        // (function () {
+        //     var iffeVar = "I'm local to the IIFE.";
+        // })();
+
 /**
- * TODO:
+ * TODO: 1
  * Create a function called 'sayHello' that takes a parameter 'name'.
  * When called, the function should return a message that says hello to the passed in name.
  *
@@ -11,33 +16,42 @@ alert('Welcome to my Website!');
  * > sayHello("codeup") // returns "Hello, codeup!"
  */
 
-function sayHello(name) {
-    console.log ("Hello, " + name)
-}
+    function sayHello(name) {
+        return("Hello, " + name);
+    };
 
-sayHello("codeup");
+    sayHello("codeup");
+
 
 /**
- * TODO:
+ * TODO: 2
  * Call the function 'sayHello' and pass your name as a string literal argument.
  * Store the result of the function call in a variable named 'helloMessage'.
  *
  * console.log 'helloMessage' to check your work
  */
 
+    let helloMessage = sayHello("Javier");
+    console.log(helloMessage);
+
 /**
- * TODO:
+ * TODO: 3
  * Store your name as a string in a variable named 'myName', and pass that
  * variable to the 'sayHello' function. You should see the same output in the
  * console.
  */
+
+    let myName = "Javier P Estrada";
+    sayHello(myName);
+
+    console.log("Hello again: " + myName);
 
 // Don't modify the following line, it generates a random number between 1 and 3
 // and stores it in a variable named random
 var random = Math.floor((Math.random() * 3) + 1);
 
 /**
- * TODO:
+ * TODO: 4
  * Create a function called 'isTwo' that takes a number as a parameter.
  * The function should return a boolean value based on whether or not the passed
  * number is the number 2.
@@ -53,9 +67,24 @@ var random = Math.floor((Math.random() * 3) + 1);
  * different result everytime you refresh the page if you are using the random
  * number)
  */
+        // example of using a IFFE
+        // (function () {
+        //     var iffeVar = "I'm local to the IIFE.";
+        // })();
+
+    function isTwo(x) {
+        if (x === 2) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    console.log(random);
+    console.log(isTwo(random));
 
 /**
- * TODO:
+ * TODO: 5
  * Create a function named 'calculateTip' to calculate a tip on a bill at a
  * restaurant. The function should accept a tip percentage and the total of the
  * bill, and return the amount to tip
@@ -66,15 +95,27 @@ var random = Math.floor((Math.random() * 3) + 1);
  * > calculateTip(0.15, 33.42) // returns 5.013
  */
 
+    function calculateTip( tipPercentage, totalBill ) {
+        return (totalBill * tipPercentage);
+    };
+
+    calculateTip(0.25, 25.50);
+    console.log(calculateTip(0.25, 25.50));
+
 /**
- * TODO:
+ * TODO: 6
  * Use prompt and alert in combination with your calculateTip function to
  * prompt the user for the bill total and a percentage they would like to tip,
  * then display the dollar amount they should tip
  */
 
+    let tipPercentage = prompt("How much do you want to tip?");
+    let totalBill     = prompt("What is the total bill?");
+    console.log("Total amount due: $" + calculateTip(tipPercentage, totalBill));
+
+
 /**
- * TODO:
+ * TODO: 7
  * Create a function named `applyDiscount`. This function should accept a price
  * (before a discount is applied), and a discount percentage (a number between 0
  * and 1). It should return the result of applying the discount to the original
@@ -82,8 +123,27 @@ var random = Math.floor((Math.random() * 3) + 1);
  *
  * Example:
  * > var originalPrice = 100;
- * > var dicountPercent = .2; // 20%
+ * > var discountPercent = .2; // 20%
  * > applyDiscount(originalPrice, dicountPercent) // 80
  *
  * > applyDiscount(45.99, 0.12) // 40.4712
  */
+
+    // let originalPrice   = prompt("What is the original price of this item?");
+    // let discountPercent = prompt("What is the discount percentage?");
+
+
+
+    function applyDiscount(x, y) {
+        let price = x;
+        let pecentOff = y;
+        let discountPrice = price - (price * pecentOff)
+
+    };
+
+    console.log(applyDiscount(45.99, .12));
+
+    // console.log(applyDiscount(10, .1), "1");
+    // console.log(applyDiscount(100, .5), "1");
+    // console.log(applyDiscount(1000, .8), "80");
+    // console.log(applyDiscount(123456, .3), "37036.799999999996");
