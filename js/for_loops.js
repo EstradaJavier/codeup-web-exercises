5e429807c071a63f186d72eafccebb2c991d2af7
javier estrada <estrada.javier.2022@gmail.com>
2020-12-18 14:54:56 -0600

    created 3 js files for the loops assignment

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,2 +1,78 @@
- // js file
- alert("Hi! Javier's JS Page.");
+// js file
+"use strict";
+alert ( "Hi! Javier's for_loops.js Page." );
+
+// For Loops
+// Create a file named for_loops.js inside the js directory and link it to your loops.html file.
+//
+// Create a function named showMultiplicationTable that accepts a number and console.logs
+// the multiplication table for that number (just multiply by the numbers 1 through 10)
+
+    // (just multiply by the numbers 1 through 10)
+    // 7 x 1 = 7
+    // 7 x 2 = 14
+    // 7 x 3 = 21
+    // 7 x 4 = 28
+    // 7 x 5 = 35
+    // 7 x 6 = 42
+    // 7 x 7 = 49
+    // 7 x 8 = 56
+    // 7 x 9 = 63
+    // 7 x 10 = 70
+
+    function showMultiplicationTable() {
+        let i = 10;
+        while (i <= 10) {
+            console.log(num + " * " + i + " = " + (num * i));
+            i++;
+        }
+    }
+
+
+
+
+    // Use a for loop and the code from the previous lessons to generate 10 random numbers
+    // between 20 and 200 and output to the console whether each number is odd or even. For example:
+        // 123 is odd
+        // 80 is even
+        // 24 is even
+        // 199 is odd
+        // ...
+
+
+    // Create a for loop that uses console.log to create the output shown below.
+        // 1
+        // 22
+        // 333
+        // 4444
+        // 55555
+        // 666666
+        // 7777777
+        // 88888888
+        // 999999999
+
+
+    // Create a for loop that uses console.log to create the output shown below.
+        // 100
+        // 95
+        // 90
+        // 85
+        // 80
+        // 75
+        // 70
+        // 65
+        // 60
+        // 55
+        // 50
+        // 45
+        // 40
+        // 35
+        // 30
+        // 25
+        // 20
+        // 15
+        // 10
+        // 5
+
+
+
