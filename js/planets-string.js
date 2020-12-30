01e12c397326faf6c2423b0263d9c0943a5455d3
javier estrada <estrada.javier.2022@gmail.com>
2020-12-30 10:31:27 -0600

    baselining this file

--- js/planets-string.js
+++ js/planets-string.js
@@ -1,3 +1,43 @@
-    alert("Hi! Javier's JS Page.");
-    
-    "use strict"
+alert ( "Hi! Javier's JS Page." );
+
+(function(){
+    "use strict";
+
+    var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+    var planetsArray;
+
+    /**
+     * TODO:
+     * Convert planetsString to an array, and save it in a variable named
+     * planetsArray.
+     * console.log planetsArray to check your work
+     */
+
+    // console.log(planetsArray);
+
+    /**
+     * TODO:
+     * Create a string with <br> tags between each planet. console.log() your
+     * results. Why might this be useful?
+     *
+     * BONUS:
+     * Create another string that would display your planets in an undordered
+     * list. You will need an opening AND closing <ul> tags around the entire
+     * string, and <li> tags around each planet.
+     */
+
+})();
+
+/** / ENCAPSULATION : encapsulation example / **/
+
+let employee = {
+
+    baseSalary: 30_000;
+    overtime: 10;
+    rate: 20;
+
+    getWage: function () {
+        return this.baseSalary + (this.overtime * this.rate);
+    }
+};
+
