24995b390d0173090a43876a9d37dbebea2fd577
javier estrada <estrada.javier.2022@gmail.com>
2020-12-30 19:05:33 -0600

    simple calculator using DOM

--- MyExtraWork/js/multiplicationDivision.js
+++ MyExtraWork/js/multiplicationDivision.js
@@ -1,3 +1,31 @@
-    alert("Hi! Javier's JS Page.");
-    
-    "use strict"
+alert ( "Hi! Javier's JS Page." );
+
+"use strict"
+
+function multiplyBy()
+{
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function divideBy()
+{
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = num1 / num2;
+}
+
+function add()
+{
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = (Number(num1) + Number(num2));
+}
+
+function subtract()
+{
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = (Number(num1) - Number(num2));
+}
