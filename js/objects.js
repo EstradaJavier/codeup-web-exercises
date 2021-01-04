d74ac80a7f08a7e60cc680d75511dba5290e1866
javier estrada <estrada.javier.2022@gmail.com>
2021-01-04 17:16:33 -0600

    completed objects.js and objects.html assignment

--- js/objects.js
+++ js/objects.js
@@ -1,3 +1,160 @@
-    alert("Hi! Javier's JS Page.");
-    
-    "use strict"
+alert ( "Hi! Javier's JS Page." );
+
+(function() {
+    "use strict";
+
+    /**
+     * TODO:
+     * Create an object with firstName and lastName properties that are strings
+     * with your first and last name. Store this object in a variable named
+     * `person`.
+     *
+     * Example:
+     *  > console.log(person.firstName) // "Rick"
+     *  > console.log(person.lastName) // "Sanchez"
+     */
+
+    const person = {
+        firstName: 'Javier',
+        lastName: 'Estrada',
+        age: 57,
+        gender: 'male',
+        interests: ['tennis', 'sand volleyball'],
+        bio: function () {
+            alert ( this.firstName[0] + ' ' + this.lastName[1] + ' is ' + this.age + ' years old. He likes ' + this.interests[0] + ' and ' + this.interests[1] + '.' );
+        },
+        greeting: function () {
+            alert ( 'Hi! I\'m ' + this.name[0] + '.' );
+        },
+        sayHello: function () {
+            alert("Hellooo from " + this.firstName + " " + this.lastName + "!");
+
+        }
+    };
+    console.log(person.sayHello());
+
+    // console.log(person);
+    // console.log(person.name);
+    // console.log(person.name[0]);
+    // console.log(person.age);
+    // console.log(person.interests[1]);
+    // console.log(person.bio);
+    // console.log(person.greeting);
+
+    /**
+     * TODO:
+     * Add a sayHello method to the person object that returns a greeting using
+     * the firstName and lastName properties.
+     * console.log the returned message to check your work
+     *
+     * Example
+     * > console.log(person.sayHello()) // "Hello from Rick Sanchez!"
+     */
+
+
+    // log the make/model
+        console.log(person.firstName);
+        console.log(person.lastName);
+
+    /** TODO:
+     * HEB has an offer for the shoppers that buy products amounting to
+     * more than $200. If a shopper spends more than $200, they get a 12%
+     * discount. Write a JS program, using conditionals, that logs to the
+     * browser, how much Ryan, Cameron and George need to pay. We know that
+     * Cameron bought $180, Ryan $250 and George $320. Your program will have to
+     * display a line with the name of the person, the amount before the
+     * discount, the discount, if any, and the amount after the discount.
+     *
+     * Uncomment the lines below to create an array of objects where each object
+     * represents one shopper. Use a foreach loop to iterate through the array,
+     * and console.log the relevant messages for each person
+     */
+
+    // var shoppers = [
+    //     {name: 'Cameron', amount: 180},
+    //     {name: 'Ryan', amount: 250},
+    //     {name: 'George', amount: 320}
+    // ];
+
+    /** TODO:
+     * Create an array of objects that represent books and store it in a
+     * variable named `books`. Each object should have a title and an author
+     * property. The author property should be an object with properties
+     * `firstName` and `lastName`. Be creative and add at least 5 books to the
+     * array
+     *
+     * Example:
+     * > console.log(books[0].title) // "The Salmon of Doubt"
+     * > console.log(books[0].author.firstName) // "Douglas"
+     * > console.log(books[0].author.lastName) // "Adams"
+     */
+
+    /**
+     * TODO:
+     * Loop through the books array and output the following information about
+     * each book:
+     * - the book number (use the index of the book in the array)
+     * - the book title
+     * - author's full name (first name + last name)
+     *
+     * Example Console Output:
+     *
+     *      Book # 1
+     *      Title: The Salmon of Doubt
+     *      Author: Douglas Adams
+     *      ---
+     *      Book # 2
+     *      Title: Walkaway
+     *      Author: Cory Doctorow
+     *      ---
+     *      Book # 3
+     *      Title: A Brief History of Time
+     *      Author: Stephen Hawking
+     *      ---
+     *      ...
+     */
+
+    /**
+     * Bonus:
+     * - Create a function named `createBook` that accepts a title and author
+     *   name and returns a book object with the properties described
+     *   previously. Refactor your code that creates the books array to instead
+     *   use your function.
+     * - Create a function named `showBookInfo` that accepts a book object and
+     *   outputs the information described above. Refactor your loop to use your
+     *   `showBookInfo` function.
+     */
+
+    function createBook (x) {
+        return x;
+    }
+
+})();
+
+// var cars = [
+//     {
+//         make: "Toyota",
+//         model: "Camry",
+//         features: ["Automatic Windows", "Bluetooth Connectivity", "GPS Navigation"],
+//         owner: {
+//             name: "Jane Doe",
+//             age: 30
+//         }
+//     },
+//     {
+//         make: "Honda",
+//         model: "Accord",
+//         features: ["Automatic Windows", "Great Gas Mileage", "AM/FM Radio"],
+//         owner: {
+//             name: "John Doe",
+//             age: 31
+//         }
+//     }
+// ];
+//
+// console.log("Here are all the features of all the cars:");
+// cars.forEach(function(car) {
+//     car.features.forEach(function(feature) {
+//         console.log(feature);
+//     });
+// });
