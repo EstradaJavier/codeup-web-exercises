bbd2022e3f2e6737670f71aed9551e2cbe6ef512
javier estrada <estrada.javier.2022@gmail.com>
2021-02-03 06:46:20 -0600

    completed jquery exercises

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,8 +1,30 @@
-(function() {
-    "use strict";
-    alert("Yes! We reached Javier's JS Page.)
-    
-        
+"use strict";
+$(document).ready(function() {
 
+    /** JQUERY EXERISES */
 
-})();
+    // alert("Page loading complete");
+
+    /** JQUERY SELECTORS LESSON EXAMPLES */
+
+    // alert($("#first-heading").html());
+
+    // alert(document.getElementById("first-heading").innerHTML);
+
+    // alert($("#second-heading").html());
+
+    // $(".codeup").css("border", "1px solid red");
+
+    $("li").css("font-size", "20px");
+
+    $("h1").css("background-color", "#ef0");
+    $("p").css("background-color", "palevioletred");
+    $("li").css("background-color", "skyblue");
+
+    $("h1").each(function () {
+        alert($("h1").html());
+    });
+
+    $("h1, p, li").css({"background-color": "darkred",
+        "border": "solid 1px gold", "color": "white"});
+});
