0769b5c802ebe2b6c2967fa97228b0b79cd11087
javier estrada <estrada.javier.2022@gmail.com>
2021-02-04 08:08:03 -0600

    adding my konami js

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,6 +1,5 @@
 "use strict";
 $(document).ready(function() {
-
     /** JQUERY EXERISES */
 
     // alert("Page loading complete");
@@ -14,17 +13,72 @@ $(document).ready(function() {
     // alert($("#second-heading").html());
 
     // $(".codeup").css("border", "1px solid red");
+    //
+    // $("li").css("font-size", "20px");
+    //
+    // $("h1").css("background-color", "#ef0");
 
-    $("li").css("font-size", "20px");
+    // $("p").css("background-color", "palevioletred");
 
-    $("h1").css("background-color", "#ef0");
-    $("p").css("background-color", "palevioletred");
-    $("li").css("background-color", "skyblue");
+    // $("li").css("background-color", "skyblue");
+    //
+    // $("h1").each(function () {
+    //     alert($("h1").html());
+    // });
+    //
+    // $("h1, p, li").css({"background-color": "darkred",
+    //     "border": "solid 1px gold", "color": "white"});
 
-    $("h1").each(function () {
-        alert($("h1").html());
-    });
 
-    $("h1, p, li").css({"background-color": "darkred",
-        "border": "solid 1px gold", "color": "white"});
+    // Class Selectors
+    //
+    //     1 - Remove your custom jQuery code from previous exercises.
+    //     2 - Update your code so that at least 3 different elements have the same class named codeup.
+    //     3 - Using jQuery, create a border around all elements with the class codeup that
+    //     is 1 pixel wide and red.
+
+           // $('.codeup').css('border', 'red');
+            $().click(function()) {
+                $(this).css("background-color", "red")
+             });
+
+    //
+    //     4 - Remove the class from one of the elements. Refresh and test that the border has been removed.
+
+
+    //
+    //     5 - Give another element an id of codeup. Does this element get a border now?
+
+
+
+    //
+    //     Element Selectors
+    //
+    //     1 - Remove your custom jQuery code from previous exercises.
+
+
+    //
+    //     2 - Using jQuery, set the font-size of all li elements to 20px.
+            $('li').css('font-size', '20px');
+
+
+    //
+    //     3 - Craft selectors that highlight all the h1, p, and li elements.
+
+
+    //
+    //     4 - Create a jQuery statement to alert the contents of your h1 element(s).
+
+
+    //
+    //     Multiple Selectors
+    //
+    //     1 - Combine your selectors that highlight all the h1, p, and li elements.
+
+
+
+
+
+
+
 });
