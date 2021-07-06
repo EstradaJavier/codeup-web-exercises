da0e775907b952cfe2e98b16c6363907d5d53a1c
javier estrada <estrada.javier.2022@gmail.com>
2021-07-06 11:45:07 -0500

    saving old work that was not saved

--- MyExtraWork/movie/js/main.js
+++ MyExtraWork/movie/js/main.js
@@ -8,7 +8,7 @@ $(document).ready(() => {
 });
 
 function getMovies(searchText){
-    axios.get('http://www.omdbapi.com?s='+searchText)
+    .get('http://www.omdbapi.com?s='+searchText)
         .then((response) => {
             console.log(response);
             let movies = response.data.Search;
