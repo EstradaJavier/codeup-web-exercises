08b19701076c6096c524ea101a2e69fab98c3d55
Javier Estrada <javierestrada@Javiers-MacBook-Pro.local>
2021-09-07 09:12:58 -0500

    feat saving unsaved files

---
+++ MyExtraWork/js/map-filter-reduce.js
@@ -0,0 +1,55 @@
+    alert("JS Loaded");
+
+    const users = [
+        {
+            id: 1,
+            name: 'ryan',
+            email: 'ryan@codeup.com',
+            languages: ['clojure', 'javascript'],
+            yearsOfExperience: 5
+        },
+        {
+            id: 2,
+            name: 'luis',
+            email: 'luis@codeup.com',
+            languages: ['java', 'scala', 'php'],
+            yearsOfExperience: 6
+        },
+        {
+            id: 3,
+            name: 'zach',
+            email: 'zach@codeup.com',
+            languages: ['javascript', 'bash'],
+            yearsOfExperience: 7
+        },
+        {
+            id: 4,
+            name: 'fernando',
+            email: 'fernando@codeup.com',
+            languages: ['java', 'php', 'sql'],
+            yearsOfExperience: 8
+        },
+        {
+            id: 5,
+            name: 'justin',
+            email: 'justin@codeup.com',
+            languages: ['html', 'css', 'javascript', 'php'],
+            yearsOfExperience: 9
+        }
+    ];
+
+
+    // const users = [
+    // const lastedTenYears = companies.filter(company => (company.end - company.start >= 10));
+
+    // Problem #2 - filter  //
+    const filterUsers = users.filter(user => user.languages.length > 2);
+
+    // Problem #3 - map  //
+    const stringUsers = users.map(user => user.email);
+    // console.log(stringUsers);
+
+    // Problem #4 - reduce  //
+      // const ageSum = ages.reduce((total, age) => total + age, 0);
+    const totalYears = users.reduce((total, user) => total);
+    console.log(totalYears)
